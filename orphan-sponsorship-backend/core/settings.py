c973ae1a422f848ddb0664815fef241f097fd585
amaheen806 <amaheen806@gmail.com>
2026-09-25 03:41:55 +0500

    Add render host to allowed hosts

--- orphan-sponsorship-backend/core/settings.py
+++ orphan-sponsorship-backend/core/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = config('SECRET_KEY', default='django-insecure-change-this-key-in-pr
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=True, cast=bool)
 
-ALLOWED_HOSTS = ['192.168.239.110', 'localhost','127.0.0.1']
+ALLOWED_HOSTS = ['192.168.239.110', 'localhost', '127.0.0.1', '.onrender.com']
 
 
 # Application definition
