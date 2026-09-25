d41667a4fdd8bd8768d099548af0c63b60ff7dc2
amaheen806 <amaheen806@gmail.com>
2026-09-25 05:06:01 +0500

    Add whitenoise for static files on Render + add db backup fixture

--- orphan-sponsorship-backend/core/settings.py
+++ orphan-sponsorship-backend/core/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'corsheaders.middleware.CorsMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -119,6 +120,8 @@ USE_TZ = True
 
 # Static and media files
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
